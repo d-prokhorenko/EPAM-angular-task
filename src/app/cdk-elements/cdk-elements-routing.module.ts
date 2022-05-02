aebb246773d7a74083fa85b1312711c3529c9ac9
Denis Prokhorenko <denis07011997q@gmail.com>
2022-05-02 12:38:37 +0300

    feat: add table cdk

--- src/app/cdk-elements/cdk-elements-routing.module.ts
+++ src/app/cdk-elements/cdk-elements-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableComponent } from './components/table/table.component';
 import { CdkPageComponent } from './pages/cdk-page/cdk-page.component';
 
-const routes: Routes = [{ path: '', component: CdkPageComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: CdkPageComponent,
+    children: [{ path: 'table', component: TableComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/cdk-elements/cdk-elements.module.ts
+++ src/app/cdk-elements/cdk-elements.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CdkElementsRoutingModule } from './cdk-elements-routing.module';
 import { CdkPageComponent } from './pages/cdk-page/cdk-page.component';
+import { TableComponent } from './components/table/table.component';
 
+import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
-  declarations: [
-    CdkPageComponent
-  ],
-  imports: [
-    CommonModule,
-    CdkElementsRoutingModule
-  ]
+  declarations: [CdkPageComponent, TableComponent],
+  imports: [CommonModule, CdkElementsRoutingModule, CdkTableModule],
 })
-export class CdkElementsModule { }
+export class CdkElementsModule {}
